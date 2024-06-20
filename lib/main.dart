a330c11c2d79b2257e67de2523376c0f15c902d8
Pedro Alves <pedromacedo2603@outlook.com>
2024-06-20 09:49:41 -0300

    Criada as funcionalidades de copiar, colar e renomear arquivos e diretórios

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,11 @@ class _HomeState extends State<Home> {
   List<FileSystemEntity> subDiretorios = [];
   Directory? diretorioAtual;
 
+  bool modoSelecao = false;
+  bool modoCopiar = false;
+  bool modoColar = false;
+  List<FileSystemEntity> entidadesSelecionadas = [];
+
   @override
   void initState() {
     super.initState();
@@ -64,37 +69,35 @@ class _HomeState extends State<Home> {
     }
   }
 
-  Future<void> criarArquivo(String nomeArquivo) async {
-    File novoArquivo = File("${diretorioAtual!.path}/$nomeArquivo");
+  Future<void> criarArquivo(String nomeArquivo, String caminhoCriar) async {
+    File novoArquivo = File("$caminhoCriar/$nomeArquivo");
 
     if (!await novoArquivo.exists()) {
       await novoArquivo.create(recursive: true);
     } else {
-      File novoArquivoDiferente = File(
-          "${diretorioAtual!.path}/$nomeArquivo-${DateTime.now().toString()}.txt");
+      File novoArquivoDiferente =
+          File("$caminhoCriar/$nomeArquivo-${DateTime.now().toString()}");
       await novoArquivoDiferente.create(recursive: true);
     }
 
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-          content: Text('Arquivo criado em: ${diretorioAtual.toString()}')),
+      SnackBar(content: Text('Arquivo criado em: $caminhoCriar')),
     );
   }
 
-  Future<void> criarDiretorio(String nomeDiretorio) async {
-    Directory novoDiretorio =
-        Directory("${diretorioAtual!.path}/$nomeDiretorio");
+  Future<void> criarDiretorio(String nomeDiretorio, String caminhoCriar) async {
+    Directory novoDiretorio = Directory("$caminhoCriar/$nomeDiretorio");
 
     if (!await novoDiretorio.exists()) {
       await novoDiretorio.create(recursive: true);
     } else {
       Directory novoDiretorioDiferente = Directory(
-          "${diretorioAtual!.path}/$nomeDiretorio-${DateTime.now().toString()}");
+          "$caminhoCriar/$nomeDiretorio-${DateTime.now().toString()}");
       await novoDiretorioDiferente.create(recursive: true);
     }
 
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Pasta criada em: ${diretorioAtual.toString()}')),
+      SnackBar(content: Text('Pasta criada em: $caminhoCriar')),
     );
   }
 
@@ -126,7 +129,32 @@ class _HomeState extends State<Home> {
 
   Future<void> moverArquivo() async {}
   Future<void> moverDiretorio() async {}
-  Future<void> copiarArquivo() async {}
+  Future<void> copiarArquivo(
+      String caminhoOriginal, String caminhoCopiar) async {
+    File novoArquivo = File(caminhoOriginal);
+
+    String nomeArquivo = caminhoOriginal.split("/").last;
+
+    await novoArquivo.copy("$caminhoCopiar/$nomeArquivo");
+  }
+
+  Future<void> colarEntidades(
+      String raiz, List<FileSystemEntity> diretorioCopiar) async {
+    for (FileSystemEntity entidade in diretorioCopiar) {
+      if (entidade is File) {
+        await copiarArquivo(entidade.path, raiz);
+      } else if (entidade is Directory) {
+        String nomeDiretorio = entidade.path.split("/").last;
+        String novoDiretorioPath = "$raiz/$nomeDiretorio";
+        await criarDiretorio(nomeDiretorio, raiz);
+
+        List<FileSystemEntity> conteudoDiretorioOriginal = entidade.listSync();
+
+        await colarEntidades(novoDiretorioPath, conteudoDiretorioOriginal);
+      }
+    }
+  }
+
   Future<void> copiarDiretorio() async {}
 
   Future<void> renomearArquivo(File arquivo, String novoNome) async {
@@ -161,7 +189,8 @@ class _HomeState extends State<Home> {
 
   Future<void> propriedades(FileSystemEntity entidade) async {
     String caminho = entidade.path.split('/').last;
-    DateTime dataModificacao = await entidade.stat().then((stat) => stat.modified);
+    DateTime dataModificacao =
+        await entidade.stat().then((stat) => stat.modified);
     int tamanho = await entidade.stat().then((stat) => stat.size);
     String tipo = entidade is File ? "arquivo" : "pasta";
 
@@ -182,7 +211,7 @@ class _HomeState extends State<Home> {
     } else {
       tamanhoMedida = '$tamanho bytes';
     }
-    
+
     Map<String, String> propriedades = {
       "caminho": caminho,
       "localização": entidade.path,
@@ -195,92 +224,203 @@ class _HomeState extends State<Home> {
       List<String> extensao = entidade.path.split(".");
 
       if (extensao.isNotEmpty) {
-        propriedades["extensão"] = extensao[extensao.length -1];
+        propriedades["extensão"] = extensao[extensao.length - 1];
       }
     }
 
-    await GerenciadorDeDialogo.mostrarDialogoPropriedades(context, propriedades);
+    await GerenciadorDeDialogo.mostrarDialogoPropriedades(
+        context, propriedades);
   }
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> acoesPersistentes() {
+      return [
+        TextButton(
+          onPressed: () async {
+            String? nomeArquivo =
+                await GerenciadorDeDialogo.mostrarDialogoInput(context);
+            if (nomeArquivo.isNotEmpty) {
+              criarArquivo(nomeArquivo, diretorioAtual!.path);
+            }
+          },
+          child: const Icon(Icons.note_add),
+        ),
+        TextButton(
+          onPressed: () async {
+            String? nomeDiretorio =
+                await GerenciadorDeDialogo.mostrarDialogoInput(context);
+            if (nomeDiretorio.isNotEmpty) {
+              criarDiretorio(nomeDiretorio, diretorioAtual!.path);
+            }
+          },
+          child: const Icon(Icons.create_new_folder),
+        )
+      ];
+    }
+
+    List<Widget> acoesPersistentesSelecao() {
+      return [
+        TextButton(
+          onPressed: () {
+            setState(() {
+              modoCopiar = true;
+              modoSelecao = false;
+            });
+          },
+          child: const Icon(Icons.copy),
+        ),
+        TextButton(
+          onPressed: () async {
+            setState(() {
+              modoSelecao = false;
+              modoColar = true;
+            });
+          },
+          child: const Icon(Icons.drive_file_move_outline),
+        ),
+        TextButton(
+          onPressed: () async {
+            await propriedades(entidadesSelecionadas.elementAt(0));
+            setState(() {
+              modoSelecao = false;
+              entidadesSelecionadas.clear();
+            });
+          },
+          child: const Icon(Icons.info),
+        ),
+        TextButton(
+          onPressed: () async {
+            String? nome =
+                await GerenciadorDeDialogo.mostrarDialogoInput(context);
+            if (nome.isNotEmpty) {
+              if (entidadesSelecionadas.elementAt(0) is File) {
+                renomearArquivo(entidadesSelecionadas.elementAt(0) as File, nome);
+              }
+              else if (entidadesSelecionadas.elementAt(0) is Directory) {
+                renomearDiretorio(entidadesSelecionadas.elementAt(0) as Directory, nome);
+              }
+            }
+
+            setState(() {
+              modoSelecao = false;
+              entidadesSelecionadas.clear();
+            });
+          },
+          child: const Icon(Icons.drive_file_rename_outline),
+        ),
+        TextButton(
+          onPressed: () {
+            for (FileSystemEntity entidade in entidadesSelecionadas) {
+              if (entidade is File) {
+                deletarArquivo(entidade);
+              } else if (entidade is Directory) {
+                deletarDiretorio(entidade);
+              }
+            }
+            setState(() {
+              modoSelecao = false;
+              entidadesSelecionadas.clear();
+            });
+          },
+          child: const Icon(Icons.delete),
+        ),
+      ];
+    }
+
+    List<Widget> acoesPersistentesCopiarColar() {
+      return [
+        TextButton(
+            onPressed: () async {
+              await colarEntidades(diretorioAtual!.path, entidadesSelecionadas);
+
+              if (modoColar) {
+                for (FileSystemEntity entidade in entidadesSelecionadas) {
+                  if (entidade is File) {
+                    deletarArquivo(entidade);
+                  } else if (entidade is Directory) {
+                    deletarDiretorio(entidade);
+                  }
+                }
+              }
+
+              setState(() {
+                entidadesSelecionadas.clear();
+                modoCopiar = false;
+                modoColar = false;
+              });
+            },
+            child: const Icon(Icons.paste)),
+        TextButton(
+          onPressed: () {
+            setState(() {
+              modoCopiar = false;
+              modoColar = false;
+              entidadesSelecionadas.clear();
+            });
+          },
+          child: const Icon(Icons.cancel),
+        )
+      ];
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(diretorioAtual?.path ?? 'Gerenciador de Arquivos'),
-        leading: diretorioAtual != null &&
-                diretorioAtual!.parent.path != diretorioAtual!.path
-            ? IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () {
-                  listarSubDiretorios(diretorioAtual!.parent);
-                },
-              )
-            : null,
-      ),
+          title: Text(modoSelecao
+              ? "Selecione os itens"
+              : (modoCopiar ? "Selecione o destino" : diretorioAtual!.path)),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              if (modoSelecao) {
+                setState(() {
+                  modoSelecao = false;
+                  entidadesSelecionadas.clear();
+                });
+              } else {
+                listarSubDiretorios(diretorioAtual!.parent);
+              }
+            },
+          )),
       body: ListView.builder(
         itemCount: subDiretorios.length,
         itemBuilder: (context, index) {
           FileSystemEntity entidade = subDiretorios[index];
           return GestureDetector(
             onTap: () {
-              if (entidade is Directory) {
+              if (modoSelecao) {
                 setState(() {
-                  diretorioAtual = entidade;
-                  subDiretorios = entidade.listSync();
+                  if (entidadesSelecionadas.contains(entidade)) {
+                    entidadesSelecionadas.remove(entidade);
+                  } else {
+                    entidadesSelecionadas.add(entidade);
+                  }
+
+                  if (entidadesSelecionadas.isEmpty) {
+                    modoSelecao = false;
+                  }
                 });
-              } else if (entidade is File) {
-                setState(() {
-                  abrirArquivo(entidade);
-                });
+              } else {
+                if (entidade is Directory) {
+                  setState(() {
+                    diretorioAtual = entidade;
+                    subDiretorios = entidade.listSync();
+                  });
+                } else if (entidade is File) {
+                  setState(() {
+                    abrirArquivo(entidade);
+                  });
+                }
               }
             },
-            onLongPress: () async {
-              if (entidade is File) {
-                Map<int, Function> opcoesPossiveis = {
-                  0: () {},
-                  1: moverArquivo,
-                  2: copiarArquivo,
-                  3: renomearArquivo,
-                  4: deletarArquivo,
-                  5: propriedades,
-                };
-
-                int opcaoEscolhida =
-                    await GerenciadorDeDialogo.mostrarDialogoOpcoesArquivo(
-                        context);
-
-                if (opcaoEscolhida == 3) {
-                  String novoNome = await GerenciadorDeDialogo.mostrarDialogoInput(context);
-                  await opcoesPossiveis[opcaoEscolhida]!(entidade, novoNome);
-                }
-                else {
-                  await opcoesPossiveis[opcaoEscolhida]!(entidade);
-                }
-                
-              } else if (entidade is Directory) {
-                Map<int, Function> opcoesPossiveis = {
-                  0: () {},
-                  1: moverDiretorio,
-                  2: copiarDiretorio,
-                  3: renomearDiretorio,
-                  4: deletarDiretorio,
-                  5: propriedades,
-                };
-
-                int opcaoEscolhida =
-                    await GerenciadorDeDialogo.mostrarDialogoOpcoesDiretorio(
-                        context);
-
-                if (opcaoEscolhida == 3) {
-                  String novoNome = await GerenciadorDeDialogo.mostrarDialogoInput(context);
-                  await opcoesPossiveis[opcaoEscolhida]!(entidade, novoNome);
-                }
-                else {
-                  await opcoesPossiveis[opcaoEscolhida]!(entidade);
-                }
-              }
+            onLongPress: () {
+              setState(() {
+                modoSelecao = true;
+                entidadesSelecionadas.add(entidade);
+              });
             },
             child: ListTile(
+              selected: entidadesSelecionadas.contains(entidade),
               leading: Icon(
                 entidade is Directory ? Icons.folder : Icons.insert_drive_file,
               ),
@@ -294,31 +434,12 @@ class _HomeState extends State<Home> {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            TextButton(
-                onPressed: () async {
-                  String? nomeArquivo =
-                      await GerenciadorDeDialogo.mostrarDialogoInput(context);
-                  if (nomeArquivo.isNotEmpty) {
-                    criarArquivo(nomeArquivo);
-                  }
-                },
-                child: const Row(
-                  children: [Text("criar arquivo"), Icon(Icons.note_add)],
-                )),
-            TextButton(
-                onPressed: () async {
-                  String? nomeDiretorio =
-                      await GerenciadorDeDialogo.mostrarDialogoInput(context);
-                  if (nomeDiretorio.isNotEmpty) {
-                    criarDiretorio(nomeDiretorio);
-                  }
-                },
-                child: const Row(
-                  children: [
-                    Text("criar pasta"),
-                    Icon(Icons.create_new_folder)
-                  ],
-                )),
+            if (modoSelecao)
+              ...acoesPersistentesSelecao()
+            else if (modoCopiar || modoColar)
+              ...acoesPersistentesCopiarColar()
+            else
+              ...acoesPersistentes()
           ],
         )
       ],
